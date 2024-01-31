35f58cde053680ece7687e00ac25f12aaa8c216e
alessio giannoccaro <mrbigalessio@gmailcom>
2024-02-01 00:10:50 +0100

    salva 6

--- resources/views/email.blade.php
+++ resources/views/email.blade.php
@@ -13,9 +13,58 @@
 
 <center><h2>EMAIL</h2></center>
 
+<center>
+
+    <form style="width: 500px; height: 750px; border: solid; background-color: aqua;" action="" method="post">
+       
+        <h3>INVIACI UN EMAIL PER FARCI SAPERE LA TUA OPINIONE</h3>
+        <br>
+    
+        <!-- NOME -->
+        <div>
+            <p style=" font-size: larger;">NOME</p>
+            <label for="nome" style="cursor: text; font-size: x-large; "></label>
+            <input  type="text" name="nome" id="nome" placeholder="Inserisci il tuo nome">
+        </div>
+    
+        <!-- COGNOME -->
+        <div>
+            <p style="padding-top: 10px; font-size: larger;">COGNOME</p>
+            <label for="cognome" style="cursor: text; font-size: x-large;"></label>
+            <input type="text" name="cognome" id="cognome" placeholder="Inserisci il tuo cognome">
+        </div>
+    
+        <!-- INDIRIZZO -->
+        <div>
+            <p style="padding-top: 10px; font-size: larger;">INDIRIZZO</p>
+            <label for="indirizzo" style="cursor: text; font-size: x-large;"></label>
+            <input type="text" name="indirizzo" id="indirizzo" placeholder="Inserisci il tuo indirizzo">
+        </div>
+    
+        <!-- EMAIL -->
+        <div>
+            <p style="padding-top: 10px; font-size: larger;">EMAIL</p>
+            <label for="email" style="cursor: text; font-size: x-large;"></label>
+            <input type="text" name="email" id="email" placeholder="Inserisci la tua email">
+        </div>
+    
+        <!-- COMMENTO -->
+        <div>
+            <p style="padding-top: 10px; font-size: larger;">COMMENTO</p>
+            <label for="commento" style="cursor: text; font-size: x-large;"></label>
+            <textarea name="commento" id="commento" cols="30" rows="2" placeholder="Dici cosa ne pensi"></textarea>
+        </div>
+    
+        <!-- INVIA -->
+        <button type="submit" style="border: solid; background-color:#DCFFB7; width: 100px; height: 50px; font-size: larger; margin-top: 30px; margin-left: 250px;">INVIA</button>
+    </form>
+    
+
+</center>
 
 
-<h1>ciao</h1>
+
+
 
 
 
