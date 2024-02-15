bc277253bd59ded9c0fd7b4aef1c4003781c7079
alessio giannoccaro <mrbigalessio@gmailcom>
2024-02-16 00:07:31 +0100

    salva 16-02-2024 ore 00.05

--- resources/views/dettaglio.blade.php
+++ resources/views/dettaglio.blade.php
@@ -16,24 +16,7 @@
 
 
 
-<nav style="" class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="{{route('home')}}">HOME</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-    
-        <a class="nav-link active" aria-current="page" href="{{route('articoli')}}">ARTICOLI</a>
-        <a class="nav-link" href="{{route('contatti')}}">CONTATTI</a>
-        <a class="nav-link" href="{{route('recensioni')}}">RECENSIONI</a>
-        
-        
-      </div>
-    </div>
-  </div>
-</nav><br>
+<x-navbar/><br>
 
 <div class="row row-cols-1 row-cols-md-3 g-4 text-center">
     <div class="col mx-auto">
--- resources/views/email.blade.php
+++ resources/views/email.blade.php
@@ -12,27 +12,7 @@
 <center><h1> ESERCITAZIONE LARAVEL ALESSIO GIANNOCCARO</h1></center>
 
 <center><h2>EMAIL</h2></center>
-
-<nav style="" class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="{{route('home')}}">HOME</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-      
-          <a class="nav-link active" aria-current="page" href="{{route('articoli')}}">ARTICOLI</a>
-          <a class="nav-link" href="{{route('contatti')}}">CONTATTI</a>
-          <a class="nav-link" href="{{route('recensioni')}}">RECENSIONI</a>
-          <a class="nav-link" href="">EMAIL</a>
-          
-          
-          
-        </div>
-      </div>
-    </div>
-  </nav><br><br><br><br>
+<x-navbar/><br><br><br><br>
 
 <center>
 
